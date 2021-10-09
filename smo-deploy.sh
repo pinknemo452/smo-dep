da36f5f12cfb3c57720fd940fce74e01812e72a4
Nikita Yankovskii <89697852+justnik98@users.noreply.github.com>
2021-10-09 17:09:22 +0300

    added deploy script

---
+++ smo-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ "$1" == "install" ]
+	then
+	apt-get upadate
+	apt-get install git -y
+	git clone https://github.com/EdikGres/smo.git
+	cd ./smo
+	tar -xf dep.tar -C ~
+	cd ~/dep/tools/k8s/bin/
+	chmod 777 ./k8s-1node-cloud-init-k_1_15-h_2_17-d_cur.sh
+	./k8s-1node-cloud-init-k_1_15-h_2_17-d_cur.sh
+fi
+
+if [ "$1" == "check" ]
+	then
+	kubect get pods --all-namespaces
+	echo "There should be 9 pods running in kube-system namespace."
+fi
+
+if [ "$1" == "deploy" ]
+	then
+	cd ~/dep/smo/bin
+	chmod 777 ./install
+	./install initlocalrepo
+	echo "Take coffee"
+	./install
+fi
