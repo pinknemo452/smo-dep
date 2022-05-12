48b2b77642f1dcbd28d163129da173bfddfa9001
Your Name <you@example.com>
2022-05-12 09:16:48 +0000

    initial

--- smo-deploy.sh
+++ smo-deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-if [ "$1" == "install" ]
-	then
 	apt-get upadate
 	apt-get install git -y
 	git clone https://github.com/EdikGres/smo.git
@@ -8,21 +6,12 @@ if [ "$1" == "install" ]
 	tar -xf dep.tar -C ~
 	cd ~/dep/tools/k8s/bin/
 	chmod 777 ./k8s-1node-cloud-init-k_1_15-h_2_17-d_cur.sh
+	sed -i -e '/reboot/d' ./k8s-1node-cloud-init-k_1_15-h_2_17-d_cur.sh
 	./k8s-1node-cloud-init-k_1_15-h_2_17-d_cur.sh
-fi
 
-if [ "$1" == "check" ]
-	then
-	kubectl get pods --all-namespaces
-	echo "There should be 9 pods running in kube-system namespace."
-fi
-
-if [ "$1" == "deploy" ]
-	then
 	cd ~/dep/smo/bin
 	chmod 777 ./install
 	./install initlocalrepo
 	cp -R /root/dep/smo/bin/smo-deploy/smo-oom/kubernetes/helm/plugins/ /root/.helm
 	echo "Take coffee"
 	./install
-fi
